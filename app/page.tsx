fb6b708a414500a6e9f7828528da97a7bb3ed2ec
Samuel Oseh <samieoseh@gmail.com>
2023-08-24 09:53:35 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className='text-4xl'>Home Page</h1>
+      <h2>Welcome to the blog</h2>
+    </main>
+  )
+}
